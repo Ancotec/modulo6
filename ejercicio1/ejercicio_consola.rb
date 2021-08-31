dd5988c35b9cc1e1346101c89769a06926882376
Ancotec <leandro.vidal@ancotec.cl>
2021-08-30 22:55:19 -0400

    agregando el primer commit

---
+++ ejercicio1/ejercicio_consola.rb
@@ -0,0 +1,16 @@
+puts "¡Hola Mundo!"
+puts ("¿como estas?")
+
+puts"Ingresa tu nombre"
+nombre_usuario = gets.chomp
+
+puts "hola normal " + nombre_usuario
+puts "hola compuesto #{nombre_usuario}"
+
+puts "¿que edad tienes?"
+edad = gets.chomp
+puts "#{edad} eso es mucho tiempo"
+
+puts "¿donde vives?"
+lugar_vivienda = gets.chomp
+puts "¿donde queda #{lugar_vivienda}?"
