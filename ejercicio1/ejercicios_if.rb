51e99802b0f959c3c8bbff915b80f1f565aea3f3
Ancotec <leandro.vidal@ancotec.cl>
2021-09-01 20:44:46 -0400

    actualizacion while 01092021

---
+++ ejercicio1/ejercicios_if.rb
@@ -0,0 +1,30 @@
+/ Ejercicios if | else | elif /
+
+/ejemplo 1/
+puts "Ingresa un numero entre 1 y 10"
+numero = gets.chomp
+
+if numero == "1"
+     puts "el numero romano de "+ numero +" es : I" 
+    elsif numero == "2"
+        puts "el numero romano de "+ numero +" es : II" 
+    elsif numero == "3"
+        puts "el numero romano de "+ numero +" es : III" 
+    elsif numero == "4"
+        puts "el numero romano de "+ numero +" es : IV" 
+    elsif numero == "5"
+        puts "el numero romano de "+ numero +" es : V" 
+    elsif numero == "6"
+        puts "el numero romano de "+ numero +" es : VI" 
+    elsif numero == "7"
+        puts "el numero romano de "+ numero +" es : VII" 
+    elsif numero == "8"
+        puts "el numero romano de "+ numero +" es : VIII" 
+    elsif numero == "9"
+        puts "el numero romano de "+ numero +" es : IX" 
+    elsif numero == "10"
+        puts "el numero romano de "+ numero +" es : X" 
+else puts "SOLO NUMEROS ENTRE 1 Y  10"
+end
+
+/ejemplo 2/
