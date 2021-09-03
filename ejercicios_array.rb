a9fc5609bc3af89bfaf21341d1b24501d001833f
Ancotec <leandro.vidal@ancotec.cl>
2021-09-02 22:59:57 -0400

    inicio array

---
+++ ejercicios_array.rb
@@ -0,0 +1,18 @@
+puts "Ingresa goles del local: "
+partido = gets.chomp.to_i
+puts "Ingresa goles de la visita"
+partido = get.chomp.to_i
+
+if partido[0]<partido[1]
+    puts "gana visita"
+else
+    puts "gana local"
+end
+
+# people = []
+# num.times do
+#   print "Ingresa el nombre de la persona: "
+#   people << gets.chomp # insertamos cada persona en el arreglo
+# end
+
+# puts "La persona seleccionada es #{people.sample}"
